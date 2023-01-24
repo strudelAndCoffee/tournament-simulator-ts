ede0a4ae82629ea48f52cbfa4d890974a558ed9e
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2023-01-24 01:09:49 -0600

    add game records in main

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
+import { Player } from './classes/Player/index.js'
 import { generateStartingPlayers } from './helpers/setup.js'
 import { qualifierStage } from './stages/index.js'
 
+const GAME_RECORDS: { [index: string]: Player[] } = {}
+
 function startGame() {
   const STARTING_PLAYERS = generateStartingPlayers()
+  GAME_RECORDS.starting_players = STARTING_PLAYERS
   const { QUALIFIED_PLAYERS, DISQUALIFIED } = qualifierStage(STARTING_PLAYERS)
-  console.log(QUALIFIED_PLAYERS)
-  console.log(DISQUALIFIED)
+  GAME_RECORDS.qualified_players = QUALIFIED_PLAYERS
+  GAME_RECORDS.disqualified_players = DISQUALIFIED
   // const PLAYOFF_PLAYERS = groupStage(QUALIFIED_PLAYERS)
   // const QUARTER_FINALISTS = playoffStage(PLAYOFF_PLAYERS)
   // const SEMI_FINALISTS = quarterFinalStage(QUARTER_FINALISTS)
